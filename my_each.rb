0ae95f6b475eb7d3e39fbc7d83a1f2aa7aaa7b18
errolgh <ewatsonrei@gmail.com>
2019-03-06 16:07:23 -0500

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
-  # code here
+words = ['hi', 'hello', 'bye', 'goodbye']
+
+def my_each(words)
+  counter = 0
+  while counter < words.length do
+    yield words[counter]
+    counter += 1
+  end
+  words
 end
